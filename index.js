8579585bf567ee0a0bcc97f9263c5650d37853e2
Mauricio Yokoyama Issei <mauricio@issei.com.br>
2021-03-29 13:12:29 -0300

    add honeycomb

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 var fs = require('fs');
 var path = require('path');
+require("honeycomb-beeline")({
+  // Get this via https://ui.honeycomb.io/account after signing up for Honeycomb
+  writeKey: "1267f6cb770876516e9a8eca25a2e48f",
+  // The name of your app is a good choice to start with
+  dataset: "my-node-distributed-app",
+  serviceName: "my-node-service"
+});
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
